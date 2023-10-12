72c82b166527711b1b7bdaa6b463f649b14f5808
Nadia <nadiapaola145@gmail.com>
2023-10-11 19:54:21 -0600

    Segun yo ya esta la pantalla de Welcome

--- TauLambda/src/pages/Welcome.jsx
+++ TauLambda/src/pages/Welcome.jsx
@@ -7,29 +7,71 @@ import AvatarImage from '../images/senora.png';
 import WelcomeImage from '../images/Brands_People_OxxoGas_gasolinera.png';
 import GasImage from '../images/Gas.png';
 import HistorialImage from '../images/Historial.png';
+import LogoImage from '../images/OXXOGAS2.png';
 
 const DATA = [
   {
     id: '1',
     imagenUrl: GasImage,
+    texto: 'Carga',
   },
   {
     id: '2',
     imagenUrl: HistorialImage,
+    texto: 'Historial',
   }
 ];
 
 const ListItem = ({ item }) => {
   return(
     <TouchableOpacity>
-      <Image
-        source={item.imagenUrl}
+      <View
         style={{
-          height: 240,
-          width: 165,
-          borderRadius:20,
+          flex: 1,
+          backgroundColor: 'white',
+          borderRadius: 20,
+          borderColor: 'black',
+          borderWidth: 2,
+          height: 230,
+          width: 160,
         }}
-      />
+      >
+        <View
+          style={{
+            flex: 2,
+            padding: 10,
+            borderTopLeftRadius:20,
+            borderTopRightRadius:20,
+            margin: 4,
+          }}
+        >
+          <Image
+            source={item.imagenUrl}
+            style={{
+              width:'100%',
+              height: '100%',
+              resizeMode:'contain',
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            borderBottomLeftRadius:20,
+            borderBottomRightRadius:20,
+            flexDirection:'row',
+            justifyContent:'center',
+            alignItems:'center',
+            margin: 4,
+          }}
+        >
+          <Text
+            category='h4'
+          >
+            {item.texto}
+          </Text>
+        </View>
+      </View>
     </TouchableOpacity>
   )
 }
@@ -44,24 +86,58 @@ const Welcome = ({navigation}) => {
     ]}>
       <View style={{
         flex:1,
-        flexDirection:'column',
-        justifyContent:'center',
-        alignItems:'flex-end',
-        padding:5,
-        }}>
-        <Avatar
-          source={AvatarImage}
-          ImageComponent={ImageBackground}
-          size='large'
-        />
-      </View>
-      <View style={{
-        flex:4,
         flexDirection:'row',
-        justifyContent: 'center',
+        justifyContent:'space-between',
         alignItems:'center',
-        padding:5,
-        }}>
+        padding:4,
+        }}
+      >
+        <View
+          style={{
+            width:140,
+            height: 70,
+          }}
+        >
+          <Image
+            source={LogoImage}
+            style={{
+              width:'100%',
+              height:'100%',
+              resizeMode:'contain',
+            }}
+          />
+        </View>
+        <View
+          style={{
+            width: 70,
+            height: 70,
+            flexDirection:'row',
+            justifyContent:'center',
+            alignItems:'center',
+          }}
+        >
+          <TouchableOpacity>
+            <Avatar
+              source={AvatarImage}
+              ImageComponent={ImageBackground}
+              size='large'
+              style={{
+                borderColor:'black',
+                borderWidth:1,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          flex:4,
+          flexDirection:'row',
+          justifyContent: 'center',
+          alignItems:'center',
+          padding:5,
+        }}
+      >
         <Image
           source={WelcomeImage}
           style={{
@@ -71,22 +147,41 @@ const Welcome = ({navigation}) => {
           }}
         />
       </View>
-      <View style={{
-        flex:4,
-        flexDirection:'row',
-        justifyContent: 'center',
-        alignItems:'center',
-        padding:5,
-        }}>
-          <FlatList
-            data={DATA}
-            keyExtractor={(item) => item.id}
-            renderItem={({item}) => <ListItem item={item}/>}
-            numColumns={2}
-            columnWrapperStyle={{
-              justifyContent: 'space-between',
+      <View
+        style={{
+          flex:4,
+          flexDirection:'column',
+          justifyContent: 'space-between',
+          alignItems:'center',
+          padding:5,
+        }}
+      >
+          <View
+            style={{
+              flex: 1,
             }}
-          />
+          >
+            <Text
+              category='h2'
+            >Servicios</Text>
+          </View>
+          <View
+            style={{
+                flexDirection:'row',
+                justifyContent: 'center',
+                alignItems:'center',
+              }}
+          >
+            <FlatList
+              data={DATA}
+              keyExtractor={(item) => item.id}
+              renderItem={({item}) => <ListItem item={item}/>}
+              numColumns={2}
+              columnWrapperStyle={{
+                justifyContent: 'space-between',
+              }}
+            />
+          </View>
       </View>
     </View>
   );
