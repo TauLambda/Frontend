0944d150c9d527614ffaac5c9faa31e8d5b11e84
“MelFadanelli” <a01749483@tec.mx>
2023-10-24 18:07:23 -0600

    Creación de componentes

---
+++ TauLambda/src/components/InputField.jsx
@@ -0,0 +1,52 @@
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import React from 'react'
+
+export default function InputField({label, icon, inputType, keyboardType, fieldButtonLabel, fieldButtonFunction}) {
+
+        return (
+                <View
+                style={{ flexDirection: "row", paddingBottom: 0, marginBottom: 25 }}
+                >
+                        {icon}
+                        {inputType == 'password' ? (
+                                <TextInput
+                                placeholder={label}
+                                keyboardType={keyboardType}
+                                style={{
+                                backgroundColor: "#ccc",
+                                flex: 1,
+                                paddingVertical: 0,
+                                borderRadius: 45,
+                                paddingLeft: 10,
+                                }}
+                                secureTextEntry={true}
+                                />
+                        ) : (
+                                <TextInput
+                                placeholder={label}
+                                keyboardType={keyboardType}
+                                style={{
+                                backgroundColor: "#ccc",
+                                flex: 1,
+                                paddingVertical: 0,
+                                borderRadius: 45,
+                                paddingLeft: 10,
+                                }}
+                                />
+                        )}
+
+                        <TouchableOpacity onPress={fieldButtonFunction}>
+                                <Text
+                                style={{
+                                color: "#de2924",
+                                fontWeight: "700",
+                                paddingTop: 5,
+                                paddingLeft: 5,
+                                }}
+                                >
+                                {fieldButtonLabel}
+                                </Text>
+                        </TouchableOpacity>
+                </View>
+        )
+}
